fe7fbfafa236100c80d29022dd638d51a6c65bdc
YJJasdasdasd <ost342@naver.com>
2022-11-02 19:13:23 +0900

    수정 및 주석달기

--- project09-4.py
+++ project09-4.py
@@ -1,16 +1,22 @@
+#  행을 분리(또는 종료)하는 데 사용되는 linesep을 사용하기 위해 os 임포트
 from os import linesep
 import googletrans
 
 translator = googletrans.Translator()
 
+# 아랍어로 작성된 파일 객체 생성
 read_file_path = r"arabic.txt"
+# 한글로 번역된 글자를 저장할 파일명 설정
 write_file_path = r"ko.txt"
 
+# 객체 생성된파일 열기
 with open(read_file_path, 'r', encoding="UTF-8") as f:
     readLines = f.readlines()
 
+# 열린 파일의 글자를 한글로 변환
 for lines in readLines:
     result1 = translator.translate(lines, dest='ko')
     print(result1.text)
+    # 파일로 저장
     with open(write_file_path, 'a', encoding='UTF8') as f:
         f.write(result1.text + '\n')
--- project09.py
+++ project09.py
@@ -2,11 +2,11 @@
 import googletrans
 
 translator = googletrans.Translator()
-
+# 입력한 글을 원하는 언어로 번역해주는 코드 dest = 번역하고싶은 언어 src = 번역할 언어 src는 기본으로 auto 설정
 str1 = "행복하세요"
 result1 = translator.translate(str1, dest='ar', src='auto')
 print((f'행복하세요 => {result1.text}'))
 
 str2 = "I am happy"
-result2 = translator.translate(str2, dest='ko', src='en')
+result2 = translator.translate(str2, dest='ar', src='en')
 print(f"I am  happy => {result2.text}")
